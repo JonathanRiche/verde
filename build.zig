8af71f05232512107579acbaf7350e7461cd9ae6
JonathanRiche <4421818+JonathanRiche@users.noreply.github.com>
2026-03-26 21:56:44 -0400

    Add desktop install and release packaging

--- build.zig
+++ build.zig
@@ -31,6 +31,7 @@ fn addDesktopCommand(
     if (optimize != .Debug) {
         argv.append(b.allocator, b.fmt("-Doptimize={s}", .{@tagName(optimize)})) catch @panic("OOM");
     }
+    appendInstallArgs(b, &argv);
 
     const cmd = b.addSystemCommand(argv.items);
     cmd.setCwd(b.path("packages/desktop"));
@@ -43,4 +44,40 @@ fn addDesktopCommand(
     }
 
     return cmd;
+}
+
+fn appendInstallArgs(b: *std.Build, argv: *std.ArrayList([]const u8)) void {
+    const default_install_prefix = if (b.dest_dir != null)
+        "/usr"
+    else
+        b.build_root.join(b.allocator, &.{"zig-out"}) catch @panic("OOM");
+    defer if (b.dest_dir == null) b.allocator.free(default_install_prefix);
+
+    const default_install_path = if (b.dest_dir) |dest_dir|
+        b.pathJoin(&.{ dest_dir, default_install_prefix })
+    else
+        default_install_prefix;
+    defer if (b.dest_dir != null) b.allocator.free(default_install_path);
+
+    const default_lib_dir = b.pathJoin(&.{ default_install_path, "lib" });
+    defer b.allocator.free(default_lib_dir);
+
+    const default_exe_dir = b.pathJoin(&.{ default_install_path, "bin" });
+    defer b.allocator.free(default_exe_dir);
+
+    const default_include_dir = b.pathJoin(&.{ default_install_path, "include" });
+    defer b.allocator.free(default_include_dir);
+
+    if (!std.mem.eql(u8, b.install_prefix, default_install_prefix)) {
+        argv.appendSlice(b.allocator, &.{ "-p", b.install_prefix }) catch @panic("OOM");
+    }
+    if (!std.mem.eql(u8, b.lib_dir, default_lib_dir)) {
+        argv.appendSlice(b.allocator, &.{ "--prefix-lib-dir", b.lib_dir }) catch @panic("OOM");
+    }
+    if (!std.mem.eql(u8, b.exe_dir, default_exe_dir)) {
+        argv.appendSlice(b.allocator, &.{ "--prefix-exe-dir", b.exe_dir }) catch @panic("OOM");
+    }
+    if (!std.mem.eql(u8, b.h_dir, default_include_dir)) {
+        argv.appendSlice(b.allocator, &.{ "--prefix-include-dir", b.h_dir }) catch @panic("OOM");
+    }
 }
